a2343ad1b111a64709e52405261712f72594064f
badal odd <badalodd96@gmail.com>
2022-02-08 17:02:02 +0530

    first coo=mmit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+         Paginator::useBootstrap();
+         View::share('recent_posts',\App\Models\Post::orderBy('id','desc')->limit(2)->get());
+         View::share('popular_posts',\App\Models\Post::orderBy('views','desc')->limit(2)->get());
+       
+    }
+}
